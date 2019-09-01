4e0e1da360578a91260c81c3b941664c5580b322
Dan Thomson <dan.j.thomson@googlemail.com>
2019-09-01 16:15:31 +0100

    - Tree functions complete

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -29,4 +29,22 @@ object Tree {
     case Leaf(value) => Leaf(op(value))
     case Branch(left, right) => Branch(map(left)(op), map(right)(op))
   }
+
+  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = t match {
+    case Leaf(value) => l(value)
+    case Branch(left,right) => b(fold(left)(l)(b),fold(right)(l)(b))
+  }
+
+  def size2[A](tree:Tree[A]): Int = {
+    fold(tree)((x:A) => 1)(1 + _ + _)
+  }
+
+  def maximum2(tree:Tree[Int]): Int = {
+    fold(tree)((v:Int) => v)((x:Int,y:Int) => x max y)
+  }
+
+  def depth2[A](tree:Tree[A]): Int = {
+    fold(tree)(_ => 1)(1 + _ max _)
+  }
+
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -25,4 +25,39 @@ class TreeTest extends org.scalatest.FunSuite {
     assert(Tree.map(input)(op) == expectd)
   }
 
+  test("Tree fold - should reduce a T to a value given a function"){
+    val input = Branch(Branch(Leaf(6), Branch(Leaf(9), Leaf(10))), Branch(Leaf(7), Leaf(4)))
+    val leafFun = (y:Int) => y
+    val branchFun = (x:Int, y:Int) => x + y
+    val expected:Int = 36
+    val result:Int = Tree.fold(input)(leafFun)(branchFun)
+
+    assert(result == expected)
+  }
+
+  test("Tree size2 - should return size 1 for single Leaf"){
+    val tree = Leaf(6)
+    assert(Tree.size2(tree) == 1)
+  }
+
+  test("Tree size2 - should return size 3 for single Branch with 2 Leaves"){
+    val tree = Branch(Leaf(6), Leaf(7))
+    assert(Tree.size2(tree) == 3)
+  }
+
+  test("Tree size2 - should return the number of the branches and leaf"){
+    val tree = Branch(Branch(Leaf(6), Branch(Leaf(9), Leaf(10))), Leaf(10))
+    assert(Tree.size2(tree) == 7)
+  }
+
+  test("Tree maximum2 - should return the max value in the tree"){
+    val tree = Branch(Branch(Leaf(6), Branch(Leaf(9), Leaf(10))), Leaf(16))
+    assert(Tree.maximum2(tree) == 16)
+  }
+
+  test("Tree depth2 - should return the maximum path length from the root to any leaf"){
+    val tree = Branch(Branch(Leaf(6), Branch(Leaf(9), Leaf(10))), Branch(Leaf(7), Leaf(4)))
+    assert(Tree.depth2(tree) == 3)
+  }
+
 }
